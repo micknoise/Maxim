e1962dd0d1fa5303a6c3f48313926302f431b0f3
Matthew Downey <mattddowney@gmail.com>
2013-07-01 22:14:35 -0600

    Fixed for Firefox Nightly

--- JavaScriptTemplate/maxim.js
+++ JavaScriptTemplate/maxim.js
@@ -1,17 +1,29 @@
 /*
- * The MIT License (MIT)
- *
- * Copyright (c) 2013 Mick Grierson, Matthew Yee-King, Marco Gillies
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+The MIT License (MIT)
 
+Copyright (c) 2013 Mick Grierson, Matthew Yee-King, Marco Gillies
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the "Software"), to 
+deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in  all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 var mtof = [0, 8.661957, 9.177024, 9.722718, 10.3, 10.913383, 11.562325, 12.25, 12.978271, 13.75, 14.567617, 15.433853, 16.351599, 17.323914, 18.354048, 19.445436, 20.601723, 21.826765, 23.124651, 24.5, 25.956543, 27.5, 29.135235, 30.867706, 32.703197, 34.647827, 36.708096, 38.890873, 41.203445, 43.65353, 46.249302, 49., 51.913086, 55., 58.27047, 61.735413, 65.406395, 69.295654, 73.416191, 77.781746, 82.406891, 87.30706, 92.498604, 97.998856, 103.826172, 110., 116.540939, 123.470825, 130.81279, 138.591309, 146.832382, 155.563492, 164.813782, 174.61412, 184.997208, 195.997711, 207.652344, 220., 233.081879, 246.94165, 261.62558, 277.182617, 293.664764, 311.126984, 329.627563, 349.228241, 369.994415, 391.995422, 415.304688, 440., 466.163757, 493.883301, 523.25116, 554.365234, 587.329529, 622.253967, 659.255127, 698.456482, 739.988831, 783.990845, 830.609375, 880., 932.327515, 987.766602, 1046.502319, 1108.730469, 1174.659058, 1244.507935, 1318.510254, 1396.912964, 1479.977661, 1567.981689, 1661.21875, 1760., 1864.655029, 1975.533203, 2093.004639, 2217.460938, 2349.318115, 2489.015869, 2637.020508, 2793.825928, 2959.955322, 3135.963379, 3322.4375, 3520., 3729.31, 3951.066406, 4186.009277, 4434.921875, 4698.63623, 4978.031738, 5274.041016, 5587.651855, 5919.910645, 6271.926758, 6644.875, 7040., 7458.620117, 7902.132812, 8372.018555, 8869.84375, 9397.272461, 9956.063477, 10548.082031, 11175.303711, 11839.821289, 12543.853516, 13289.75];
-var context = new webkitAudioContext();  
+// Patch Webkit Browsers
+window.AudioContext = (
+  window.AudioContext ||
+  window.webkitAudioContext ||
+  null
+);
+var context = new AudioContext();
 
 function Maxim(t) {
 
@@ -44,7 +56,7 @@ function Maxim(t) {
         myAudioBuffer = buffer;
         //       alert("sound decoded"); //test
         source = context.createBufferSource();
-        gainNode = context.createGainNode();
+        gainNode = context.createGain();
         filter = context.createBiquadFilter();
         analyser = context.createAnalyser();
         filter.type = 0;
@@ -92,10 +104,6 @@ function Maxim(t) {
       }
     }
 
-    audio.setAnalysing = function(analysing_) {
-      //this.analysing = analysing_;
-    }
-
     audio.volume = function(gain) {
 
       volume=gain;
@@ -106,10 +114,11 @@ function Maxim(t) {
     }
 
     audio.play = function() {
-      if (source && !playing) {
+      if (source) {
+      if (!playing) {
         source = context.createBufferSource();
-        gainNode = context.createGainNode()
-          filter = context.createBiquadFilter();
+        gainNode = context.createGain()
+        filter = context.createBiquadFilter();
         filter.type = 0;
         filter.frequency.value = 20000;
         envTime = 1.0;
@@ -124,14 +133,16 @@ function Maxim(t) {
         if (isLooping) source.loop = true;
         //          source.loopStart = startTime/1000;
         //          source.loopEnd = source.buffer.duration;
-        source.noteGrainOn(0, startTime, source.buffer.duration-startTime);
+        //source.noteGrainOn(0, startTime, source.buffer.duration-startTime);
+        source.start(0, startTime, source.buffer.duration-startTime);
         playing=true;
       }
-      if (analysing==true && playing) {
-        gainNode.connect(analyser);
-        FFTData = new Float32Array(analyser.frequencyBinCount);
-        analyser.getFloatFrequencyData(FFTData);
-      }
+      if (analysing==true) {
+          gainNode.connect(analyser);
+          FFTData = new Float32Array(analyser.frequencyBinCount);
+          analyser.getFloatFrequencyData(FFTData);
+        }
+    }
     }
 
     audio.stop = function() {
@@ -168,36 +179,26 @@ function Maxim(t) {
       gainNode.gain.linearRampToValueAtTime(gainNode.gain.value, context.currentTime);
       gainNode.gain.linearRampToValueAtTime(amplitude, context.currentTime + envTime/1000.);
     }
-
+    
     audio.getAveragePower = function() {
-      if (source) {
-        averageSpectrumPower = 0
-          for (var i=0;i<analyser.frequencyBinCount;i++) {
-
-          averageSpectrumPower+=FFTData[i]
-        }
-        return (100+(averageSpectrumPower/analyser.frequencyBinCount))*0.01;
-      }
+      averageSpectrumPower = 0
+      for (var i=0;i<analyser.frequencyBinCount;i++) {
+      
+              averageSpectrumPower+=FFTData[i]
+          }
+      return (100+(averageSpectrumPower/analyser.frequencyBinCount))*0.01; 
     }
-    audio.getPowerSpectrum = function() {
-      if (source) {
-        return FFTData;
-      }
+    
+      audio.getFlux = function() {
+      flux=0;
+      var FFTData1 = new Float32Array(analyser.frequencyBinCount);
+      for (var i=0;i<analyser.frequencyBinCount;i++) {
+      
+              flux+=FFTData[i]-FFTData1[i];
+          }
+            FFTData1=FFTData;
+      return (100+(flux/analyser.frequencyBinCount))*0.01; 
     }
-
-    audio.getFlux = function() {
-      if (source) {
-        flux=0;
-        var FFTData1 = new Float32Array(analyser.frequencyBinCount);
-        for (var i=0;i<analyser.frequencyBinCount;i++) {
-
-          flux+=FFTData[i]-FFTData1[i];
-        }
-        FFTData1=FFTData;
-        return (100+(flux/analyser.frequencyBinCount))*0.01;
-      }
-    }
-
 
     return audio;
   }
@@ -217,8 +218,8 @@ Synth = function() {
     this.sample_rate = 44100;
   this.frequency = 220;
   this.amplitude = 1.0;
-  this.gainNode = context.createGainNode();
-  this.delayGain = context.createGainNode();
+  this.gainNode = context.createGain();
+  this.delayGain = context.createGain();
   this.filter = context.createBiquadFilter();
   this.delay = context.createDelayNode(2);
   this.delayAmt = 0.75;
@@ -305,15 +306,17 @@ Synth.prototype.stop = function() {
 Synth.prototype.setDelayTime = function(t) {
 
   this.delay.delayTime.value = t;
+
 }
 
 Synth.prototype.setDelayAmount = function(t) {
-
+  
   this.delayGain.gain.value = t;
 
-  //  this.delayGain.gain.cancelScheduledValues(context.currentTime);
-  //  this.delayGain.gain.linearRampToValueAtTime(this.delayGain.gain.value, context.currentTime);
-  //  this.delayGain.gain.linearRampToValueAtTime(this.delayGain.gain.value, context.currentTime,100);
+//  this.delayGain.gain.cancelScheduledValues(context.currentTime);
+//  this.delayGain.gain.linearRampToValueAtTime(this.delayGain.gain.value, context.currentTime);
+//  this.delayGain.gain.linearRampToValueAtTime(this.delayGain.gain.value, context.currentTime,100);
+
 }
 
 Synth.prototype.setFilter = function(freq, res) {
@@ -330,4 +333,3 @@ Synth.prototype.filterRamp = function(freq, envTime) {
   //  this.filter.frequency.value = freq;
   //  this.filter.Q.value = res;
 }
-
