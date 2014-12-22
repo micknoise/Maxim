e11a1e3d127a36fa7b303cb7991d916044c181a3
Willy Zhang <willxzhang@gmail.com>
2014-12-22 10:46:55 -0800

    Update maxim with new standards

--- JavaScriptTemplate/maxim.js
+++ JavaScriptTemplate/maxim.js
@@ -12,7 +12,7 @@
 
 var mtof = [0, 8.661957, 9.177024, 9.722718, 10.3, 10.913383, 11.562325, 12.25, 12.978271, 13.75, 14.567617, 15.433853, 16.351599, 17.323914, 18.354048, 19.445436, 20.601723, 21.826765, 23.124651, 24.5, 25.956543, 27.5, 29.135235, 30.867706, 32.703197, 34.647827, 36.708096, 38.890873, 41.203445, 43.65353, 46.249302, 49., 51.913086, 55., 58.27047, 61.735413, 65.406395, 69.295654, 73.416191, 77.781746, 82.406891, 87.30706, 92.498604, 97.998856, 103.826172, 110., 116.540939, 123.470825, 130.81279, 138.591309, 146.832382, 155.563492, 164.813782, 174.61412, 184.997208, 195.997711, 207.652344, 220., 233.081879, 246.94165, 261.62558, 277.182617, 293.664764, 311.126984, 329.627563, 349.228241, 369.994415, 391.995422, 415.304688, 440., 466.163757, 493.883301, 523.25116, 554.365234, 587.329529, 622.253967, 659.255127, 698.456482, 739.988831, 783.990845, 830.609375, 880., 932.327515, 987.766602, 1046.502319, 1108.730469, 1174.659058, 1244.507935, 1318.510254, 1396.912964, 1479.977661, 1567.981689, 1661.21875, 1760., 1864.655029, 1975.533203, 2093.004639, 2217.460938, 2349.318115, 2489.015869, 2637.020508, 2793.825928, 2959.955322, 3135.963379, 3322.4375, 3520., 3729.31, 3951.066406, 4186.009277, 4434.921875, 4698.63623, 4978.031738, 5274.041016, 5587.651855, 5919.910645, 6271.926758, 6644.875, 7040., 7458.620117, 7902.132812, 8372.018555, 8869.84375, 9397.272461, 9956.063477, 10548.082031, 11175.303711, 11839.821289, 12543.853516, 13289.75];
 
-var context = new (window.AudioContext || window.webkitAudioContext)();
+var context = new webkitAudioContext() || new AudioContext();
 
 function Maxim(t) {
 
@@ -50,7 +50,7 @@ function Maxim(t) {
         myAudioBuffer = buffer;
         //       alert("sound decoded"); //test
         source = context.createBufferSource();
-        gainNode = context.createGainNode();
+        gainNode = context.createGain();
         filter = context.createBiquadFilter();
         analyser = context.createAnalyser();
         filter.type = "lowpass";
@@ -75,7 +75,7 @@ function Maxim(t) {
           myAudioBuffer = buffer;
           //       alert("sound decoded"); //test
           source = context.createBufferSource();
-          gainNode = context.createGainNode();
+          gainNode = context.createGain();
           filter = context.createBiquadFilter();
           analyser = context.createAnalyser();
           filter.type = "lowpass";
@@ -140,7 +140,7 @@ function Maxim(t) {
     audio.play = function() {
       if (source && !playing) {
         source = context.createBufferSource();
-        gainNode = context.createGainNode()
+        gainNode = context.createGain();
           filter = context.createBiquadFilter();
         filter.type = "lowpass";
         filter.frequency.value = 20000;
@@ -158,7 +158,7 @@ function Maxim(t) {
         //          source.loopEnd = source.buffer.duration;
 
         startAbsTime = context.currentTime; // initialize startAbsTime to set where audio started playing.
-        source.noteGrainOn(0, startTime, source.buffer.duration-startTime);
+        source.start(0, startTime, source.buffer.duration-startTime);
         playing=true;
       }
       if (analysing==true && playing) {
@@ -171,7 +171,7 @@ function Maxim(t) {
     audio.stop = function() {
       if (source && playing) {
         startTime=0; // Reset start-timer.
-        source.noteOff(0);
+        source.stop(0);
         playing=false;
       }
     }
@@ -180,7 +180,7 @@ function Maxim(t) {
       if (source && playing) {
         // Generate future start-time offset based on start and audiocontext currenttime
         startTime = (context.currentTime - startAbsTime) + startTime;
-        source.noteOff(0);
+        source.stop(0);
         playing=false;
       }
     }
@@ -253,17 +253,17 @@ Synth = function() {
   var that = this;
   this.phase = 0;
   this.context = context;
-  this.node = context.createJavaScriptNode(512, 2, 2);
+  this.node = context.createScriptProcessor(512, 2, 2);
   this.node.onaudioprocess = function(audioContext) { 
-    that.process(audioContext)
+    that.process(audioContext);
     };
     this.sample_rate = 44100;
   this.frequency = 220;
   this.amplitude = 1.0;
-  this.gainNode = context.createGainNode();
-  this.delayGain = context.createGainNode();
+  this.gainNode = context.createGain();
+  this.delayGain = context.createGain();
   this.filter = context.createBiquadFilter();
-  this.delay = context.createDelayNode(2);
+  this.delay = context.createDelay(2);
   this.delayAmt = 0.75;
   this.delayGain.gain.value = 0.75;
   this.filter.type = "lowpass";
